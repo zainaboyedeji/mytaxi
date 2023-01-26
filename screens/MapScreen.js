6bd32b9bafaabfdbda97c29ae1f9dd204f570a0b
zainaboyedeji <ololadeoyedejiii@gmail.com>
2023-01-26 17:59:54 +0100

    new changes

---
+++ screens/MapScreen.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
+import Map from "../components/Map"
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { StackActions } from '@react-navigation/native'
+import NavigateCard from '../components/NavigateCard'
+import RideOptionsCard from '../components/RideOptionsCard'
+import { Icon } from '@rneui/base'
+
+const MapScreen = () => {
+    const Stack = createNativeStackNavigator();
+    const navigation = useNavigation()
+    return ( 
+        <View>
+            <TouchableOpacity
+                onPress={() => navigation.navigate("HomeScreen")}
+
+                style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}>
+                <Icon name="menu" />
+            </TouchableOpacity>
+            <View style={tw`h-1/2`}>
+                <Map />
+            </View>
+
+            <View style={tw`h-1/2`}>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="NavigateCard"
+                        component={NavigateCard}
+                        options={{
+                            headerShown: false,
+                        }}
+                    />
+                    <Stack.Screen
+                        name="RideOptionsCard"
+                        component={RideOptionsCard}
+                        options={{
+                            headerShown: false,
+                        }}
+                    />
+                </Stack.Navigator>
+            </View>
+        </View>
+    )
+}
+
+export default MapScreen
